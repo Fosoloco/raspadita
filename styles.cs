48da214455fe68971f4499226d0685d6365a749a
Fosoloco <159410032+Fosoloco@users.noreply.github.com>
2024-09-05 00:40:49 -0300

    Create styles.cs

---
+++ styles.cs
@@ -0,0 +1,22 @@
+body {
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f4f4f4;
+}
+
+.scratch-container {
+    position: relative;
+    width: 300px;
+    height: 300px;
+}
+
+canvas {
+    width: 100%;
+    height: 100%;
+    border: 1px solid #ccc;
+    background: url('imagen_raspadita.jpg') no-repeat center center;
+    background-size: cover;
+}
